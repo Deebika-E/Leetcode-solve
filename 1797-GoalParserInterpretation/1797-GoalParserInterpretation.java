73247767a2bdd1463d2a15daedda6f89f3033d84
Deebika-E <deebikaellappan12@gmail.com>
2026-01-24 15:26:12 +0530

    Create 1797-GoalParserInterpretation

---
+++ 1797-GoalParserInterpretation/1797-GoalParserInterpretation.java
@@ -0,0 +1,21 @@
+// Last updated: 1/24/2026, 3:26:11 PM
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='G'){
+                sb.append("G");
+            }
+            else if(command.charAt(i)=='('&&command.charAt(i+1)==')'){
+                sb.append("o");
+                i++;
+            }
+            else{
+                sb.append("al");
+                i+=3;  
+            }
+        }
+        return sb.toString();
+        
+    }
+}
