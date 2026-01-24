fa3ed40e84e4198d531f3165eee275dc00fa079c
Deebika-E <deebikaellappan12@gmail.com>
2026-01-24 15:26:17 +0530

    Create 1635-NumberOfGoodPairs

---
+++ 1635-NumberOfGoodPairs/1635-NumberOfGoodPairs.java
@@ -0,0 +1,23 @@
+// Last updated: 1/24/2026, 3:26:16 PM
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        
+        Map<Integer,Integer> freq = new HashMap();
+
+        for(int x: nums){
+            if(freq.containsKey(x)){
+                freq.put(x,freq.get(x)+1);
+            }else{
+                freq.put(x,1);
+            }
+
+        }
+
+        int pairs = 0;
+
+        for(int n:freq.values()){
+            pairs+=(n*(n-1))/2;
+        }
+        return pairs;
+    }
+}
