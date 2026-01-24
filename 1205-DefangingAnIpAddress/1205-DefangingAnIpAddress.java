97aa85783725be8d9d7d1cd3ca8e64bf930cf4aa
Deebika-E <deebikaellappan12@gmail.com>
2026-01-24 15:26:27 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.java
@@ -0,0 +1,8 @@
+// Last updated: 1/24/2026, 3:26:26 PM
+class Solution {
+    public String defangIPaddr(String address) {
+        String res= address.replace(".", "[.]");
+          return res;
+        
+    }
+}
