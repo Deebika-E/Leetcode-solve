2b59dc3da0c03a77feb5b5fe7ab0e0818c544304
Deebika-E <deebikaellappan12@gmail.com>
2026-02-01 14:22:21 +0530

    Create

---
+++ problem-1769935932399/problem-1769935932399.java
@@ -0,0 +1,18 @@
+// Last updated: 2/1/2026, 2:22:12 PM
+1class Solution {
+2    public int climbStairs(int n) {
+3        if(n<=2)
+4           return n;
+5
+6        int prev1 = 2;
+7        int prev2 = 1; 
+8
+9        for (int i = 3; i <= n; i++) {
+10            int curr = prev1 + prev2;
+11            prev2 = prev1;
+12            prev1 = curr;
+13        }
+14
+15        return prev1;
+16    }
+17}
