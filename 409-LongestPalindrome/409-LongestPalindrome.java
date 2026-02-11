c9e630cd02def1fad3dd12c48cf608745dd8d5f2
Deebika-E <deebikaellappan12@gmail.com>
2026-02-11 22:14:34 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.java
@@ -0,0 +1,28 @@
+// Last updated: 2/11/2026, 10:14:24 PM
+1class Solution {
+2    public int longestPalindrome(String s) {
+3        int[] freq = new int[128]; 
+4
+5     
+6        for (int i = 0; i < s.length(); i++) {
+7            freq[s.charAt(i)]++;
+8        }
+9
+10        int length = 0;
+11        boolean hasOdd = false;
+12
+13        for (int count : freq) {
+14            if (count % 2 == 0) {
+15                length += count;
+16            } else {
+17                length += count - 1;
+18                hasOdd = true;
+19            }
+20        }
+21
+22        if (hasOdd) length += 1;
+23
+24        return length;
+25    }
+26}
+27
