5d3b600f5b6900a2ee55471f2be245b64534bec5
Deebika-E <deebikaellappan12@gmail.com>
2026-02-28 19:07:36 +0530

    File updated at 2/28/2026, 7:07:05 PM

--- 2765-LongestAlternatingSubarray/2765-LongestAlternatingSubarray.java
+++ 2765-LongestAlternatingSubarray/2765-LongestAlternatingSubarray.java
@@ -1,23 +1,31 @@
-// Last updated: 2/28/2026, 7:01:36 PM
+// Last updated: 2/28/2026, 7:07:05 PM
 1class Solution {
 2    public int alternatingSubarray(int[] nums) {
-3        int max = -1, cnt = -1, j;
-4        if(nums.length == 2 && nums[1] - nums[0] == 1)  return 2;
-5        for(int i=2; i<=nums.length; i++){
-6            if(i == nums.length && nums[i-1] - nums[i-2] == 1){
-7                max = Math.max(max,2);
-8                break;
-9            }
-10            if(nums[i-1] - nums[i-2] == 1){
-11                cnt = 2;
-12                j = i;
-13                while(j < nums.length && nums[j] == nums[j-2]){
-14                    j++;
-15                    cnt++;
-16                }
-17                max = Math.max(max,cnt);
-18            }
-19        }
-20        return max;
-21    }
-22}
+3        int n = nums.length;
+4        int max = -1;
+5        int len = 1;
+6        int expected = 1;  
+7
+8        for (int i = 1; i < n; i++) {
+9            int diff = nums[i] - nums[i - 1];
+10
+11            if (diff == expected) {
+12                len++;
+13                expected *= -1;  
+14                max = Math.max(max, len);
+15            } else {
+16              
+17                if (diff == 1) {
+18                    len = 2;
+19                    expected = -1;
+20                    max = Math.max(max, len);
+21                } else {
+22                    len = 1;
+23                    expected = 1;
+24                }
+25            }
+26        }
+27
+28        return max;
+29    }
+30}
