51b518553a23abdb1cdf70d26bd22f82d4b60151
Deebika-E <deebikaellappan12@gmail.com>
2026-06-10 21:24:29 +0530

    Create

---
+++ problem-1781106856054/problem-1781106856054.java
@@ -0,0 +1,22 @@
+// Last updated: 6/10/2026, 9:24:16 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean checkTree(TreeNode root) {
+18       if(root==null) return true;
+19       return root.val==root.left.val+root.right.val;
+20    }
+21}
